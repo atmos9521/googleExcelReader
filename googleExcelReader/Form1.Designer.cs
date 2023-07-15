c2d852a32cef840fe7979052f6ac5b5501504d45
willychen <willy77423@gmail.com~>
2023-07-16 06:55:36 +0800

    讀取googleExcel

---
+++ googleExcelReader/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace googleExcelReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Read = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Read
+            // 
+            this.Read.Location = new System.Drawing.Point(25, 34);
+            this.Read.Name = "Read";
+            this.Read.Size = new System.Drawing.Size(237, 96);
+            this.Read.TabIndex = 0;
+            this.Read.Text = "Read";
+            this.Read.UseVisualStyleBackColor = true;
+            this.Read.Click += new System.EventHandler(this.Read_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 205);
+            this.Controls.Add(this.Read);
+            this.Name = "Form1";
+            this.Text = "googleExcelReader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Read;
+    }
+}
+
---
+++ googleExcelReader/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using System.IO;
+
+namespace googleExcelReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Nuget: google.Apis.Sheets.v4
+        }
+
+        private void Read_Click(object sender, EventArgs e)
+        {
+            //從客戶端密鑰檔案中載入憑證：
+            GoogleCredential credential;
+            using (var stream = new FileStream(@"./mimetic-plate-392915-66bde0131431.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleCredential.FromStream(stream).CreateScoped(SheetsService.Scope.Spreadsheets);
+                string sheetName = "Sheet1";
+                //建立SheetsService實例：
+                var service = new SheetsService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = sheetName
+                });
+
+                //讀取指定的Google Sheets資料：
+                // 指定要讀取的Google Sheets的ID和範圍（例如，Sheet1中的A1:C10儲存格範圍）
+                // spreadsheet_id = 網址中，spreadsheets後面
+                string spreadsheet_id = "1yejADfPXru_r9qJzGSQvrr7t9lrlouGdmGvk-vaYnYo";
+                var spreadsheetId = spreadsheet_id;
+
+                // 呼叫API進行資料讀取
+                // 處理讀取的資料
+                var lastColumn = "Z";
+                var range = $"{sheetName}!A1:{lastColumn}";
+
+                var request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+                var response = request.Execute();
+                var values = response.Values;
+
+                if (values != null && values.Count > 0)
+                {
+                    foreach (var row in values)
+                    {
+                        foreach (var cell in row)
+                        {
+                            Console.WriteLine(cell);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
